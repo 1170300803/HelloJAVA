1107752e27218b7eea25f52006b18a821ba9294a
Mirakal <zehao323@gmail.com>
2019-07-06 12:54:29 +0800

    add Constant file hit-1170300801

---
+++ OpenSource/src/helloworld/Constant.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class Constant {
+  public static final int DEFAULT_WIDTH = 300;
+  public static final int DEFAULT_HEIGHT = 200;
+  public static final int FONT_SIZE = 16;
+}
