23c67108dd82e07de6c0b340720bbb0362c64591
Mirakal <zehao323@gmail.com>
2019-07-06 12:58:03 +0800

    Update Constant.java

--- OpenSource/src/helloworld/Constant.java
+++ OpenSource/src/helloworld/Constant.java
@@ -1,7 +1,7 @@
 package helloworld;
 
 public class Constant {
-  public static final int DEFAULT_WIDTH = 300;
-  public static final int DEFAULT_HEIGHT = 200;
-  public static final int FONT_SIZE = 16;
+  public static final int DEFAULT_WIDTH = 300; // the width of the window
+  public static final int DEFAULT_HEIGHT = 200; // the height of the window
+  public static final int FONT_SIZE = 16; // the size of the font
 }
