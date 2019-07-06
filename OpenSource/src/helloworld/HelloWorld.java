48964fe7e64cde677eee868158614261bb28306c
1170300802 <1061002634@qq.com>
2019-07-06 12:49:17 +0800

    Open Source

---
+++ OpenSource/src/helloworld/HelloWorld.java
@@ -0,0 +1,68 @@
+package helloworld;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        FontFrame frame = new FontFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+      }
+    });
+  }
+}
+
+
+class FontFrame extends JFrame {
+  private static final long serialVersionUID = 1L;
+  
+  public static final int DEFAULT_WIDTH = 300;
+  public static final int DEFAULT_HEIGHT = 200;
+  
+  public FontFrame() {
+    setTitle("JAVA");
+    setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    FontComponent component = new FontComponent();
+    add(component);
+  }
+}
+
+
+class FontComponent extends JComponent {
+  private static final long serialVersionUID = 1L;
+
+  public void paintComponent(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g;
+    
+    String message = "Hello World!";
+    
+    Font font = new Font("Serif", Font.BOLD, 36);
+    g2.setFont(font);
+    
+    FontRenderContext context = g2.getFontRenderContext();
+    Rectangle2D bounds = font.getStringBounds(message, context);
+    
+    double x = (getWidth() - bounds.getWidth())/2;
+    double y = (getHeight() - bounds.getHeight())/2;
+    
+    double ascent = -bounds.getY();
+    double baseY  = y + ascent;
+    
+    g2.drawString(message, (int)x, (int)baseY);
+    
+  }
+}
+
+
+
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# hit-1170300803
+Open Source
+This is a program about printing "Hello World" in java GUI
